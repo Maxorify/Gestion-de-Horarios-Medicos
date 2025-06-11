54c036e7fc564229ee346708f1e6180d6316b521
Maxinho <maxorify@gmail.com>
2025-06-11 17:38:56 -0400

    Implementando funcionalidad de Agendar Consulta, aun nno abre el pop-up de confirmación de si el paciente anteriormente se atendió.

--- Gestion-Horarios-Medicos/src/pages/AdminPanel.jsx
+++ Gestion-Horarios-Medicos/src/pages/AdminPanel.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Topbar from "../components/Topbar";
 import Sidebar from "../components/SidebarAdmin";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
 
 function AdminPanel() {
   const navigate = useNavigate();
